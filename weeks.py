b60f8463473f586c3f509e5008dd71937fd14894
Christopher Partin <christopherpartin11@gmail.com>
2024-05-28 10:39:54 -0700

    Added a weeks class

--- weeks.py
+++ weeks.py
@@ -1,1 +1,14 @@
 
+
+class Weeks:
+    def __init__(self, week_num, day1, day2, day3, day4, day5, day6, day7):
+        self._week_num = week_num
+        self._day1 = day1
+        self._day2 = day2
+        self._day3 = day3
+        self._day4 = day4
+        self._day5 = day5
+        self._day6 = day6
+        self._day7 = day7
+
+
